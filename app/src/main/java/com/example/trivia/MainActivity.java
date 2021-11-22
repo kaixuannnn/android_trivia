1aeb3df4672b9dfe7ad5be273f5e7dccd3158f5e
Kai Xuan <kaixuan@agmostudio.com>
2021-11-22 16:02:29 +0800

    add jsonarrayrequest

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
+    String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, response -> {
+            Log.d("TAG", "onCreate: "+response.toString());
+        }, error ->{} );
     }
 }
